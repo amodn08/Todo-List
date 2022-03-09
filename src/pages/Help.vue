8c242c01b26217751a8c333992bb37de1ec2ee3e
Amod <amodnagbhidkar08@gmail.com>
2022-03-09 17:54:38 +0530

    Initial commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum aperiam modi, eligendi impedit sapiente doloremque maxime a nulla quam. Odio, aspernatur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At rem amet earum cum mollitia dicta esse neque totam tenetur repellat?</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
